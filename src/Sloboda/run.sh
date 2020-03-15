ff18a33aa549d618663e2f419c8c2b9fcc7dae89
Dmytro Gladkyi <d.gladkyi@sap.com>
2020-03-15 20:50:43 +0200

    Removed second mount argument.

--- src/Sloboda/run.sh
+++ src/Sloboda/run.sh
@@ -1,4 +1,3 @@
  docker run --name my_pharo -d -p 5901:5901 -p 6901:6901 -p 9090:9090 \
     -v=$HOME/projects/sich/Pharo/images/Pharo:/root/data \
-    -v=$HOME/projects/sich:/root/gitrepo \
     pharo
