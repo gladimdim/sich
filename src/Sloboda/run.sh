ae4e9fba563b498f1e62d0bb9ac2d00573a5ac7e
Dmytro Gladkyi <d.gladkyi@sap.com>
2020-03-15 20:56:31 +0200

    Fixed run command in run.sh to refer correct folder name with image.

--- src/Sloboda/run.sh
+++ src/Sloboda/run.sh
@@ -1,3 +1,4 @@
  docker run --name my_pharo -d -p 5901:5901 -p 6901:6901 -p 9090:9090 \
-    -v=$HOME/projects/sich/Pharo/images/Pharo:/root/data \
+    -v=$HOME/projects/sich/Pharo:/root/data \
+    -v=$HOME/projects/sich:/root/gitrepo \
     pharo
